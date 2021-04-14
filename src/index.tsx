9f79a85e71e82b10044e670f00a0804dfdc125eb
Eduardo Spada <duspada@users.noreply.github.com>
2021-04-14 09:50:45 -0300

    feat(upgrade): libs and code (#130)
    * feat(upgrade): libs and code
    
    * feat(upgrade): actions
    
    * feat(upgrade): actions
    
    * feat(upgrade): actions
    
    * feat(upgrade): versioning
    
    Co-authored-by: Eduardo Spada <eduardo.spada@platformbuilders.io>

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,60 @@
-import { Linking, Platform } from 'react-native';
-import * as StoreReview from 'react-native-store-review';
+import { Alert, Linking, Platform } from 'react-native';
+import { isAvailable, requestReview } from 'react-native-store-review';
+import VersionNumber from 'react-native-version-number';
 import { Storage } from './services';
-import { useRatingInterface, Rated } from './types';
+import { useRatingInterface, useRatingProps, Rated } from './types';
 
-export const useRating = (appVersion: string): useRatingInterface => {
+export const useRating = ({
+  iosAppId,
+  alertTitle,
+  alertMsg,
+  okButton,
+  cancelButton,
+}: useRatingProps): useRatingInterface => {
+
+  const APP_STORE_LINK = `itms-apps://apps.apple.com/app/id${iosAppId}?action=write-review`;
+  const PLAY_STORE_LINK = `market://details?id=${VersionNumber.bundleIdentifier}`;
+
+  const STORE_LINK = Platform.select({
+    ios: APP_STORE_LINK,
+    android: PLAY_STORE_LINK,
+  });
+
+  const createAlert = () => Alert.alert(
+      alertTitle || "Avalie este app",
+      alertMsg || "Clique em 'Avaliar' para ser redirecionado para a loja e avaliar nosso aplicativo!",
+      [
+        {
+          text: cancelButton || "Cancelar",
+          onPress: () => console.log("Cancel Pressed"),
+        },
+        { text: okButton || "Avaliar",
+          onPress: () => Linking.openURL(STORE_LINK as string),
+        }
+      ]
+    );
+    
   const shouldRate = async (usesToRate = 5): Promise<boolean> => {
+    const rated = await Storage.getRated();
+    if ( rated.rated && rated.appVersion === VersionNumber.appVersion ) return false;
     const uses = await Storage.getUses();
     const usedEnough = uses >= usesToRate;
-    const rated = await Storage.getRated();
-    Storage.setUses(uses + 1);
-    if (appVersion != rated.appVersion && usedEnough) {
+    await Storage.setUses(uses + 1);
+    if (VersionNumber.appVersion !== rated.appVersion && usedEnough) {
       return true;
     }
     return false;
   };
 
-  const askRate = (androidBundleId: string): void => {
+  const askRate = (): void => {
     const rateObject: Rated = {
       rated: true,
-      appVersion,
+      appVersion: VersionNumber.appVersion,
     };
-    if (StoreReview.isAvailable) {
-      StoreReview.requestReview();
-    } else if (Platform.OS === 'android') {
-      const link = `https://play.google.com/store/apps/details?${androidBundleId}`;
-      Linking.openURL(link);
+    if (isAvailable) {
+      requestReview();
+    } else {
+      createAlert();
     }
     Storage.setRated(rateObject);
   };
@@ -32,5 +62,6 @@ export const useRating = (appVersion: string): useRatingInterface => {
   return {
     shouldRate,
     askRate,
+    isAvailable,
   };
 };
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,16 @@
 export interface useRatingInterface {
   shouldRate(usesToRate?: number): Promise<boolean>;
-  askRate(androidBundleId: string): void;
+  askRate(): void;
+  isAvailable: boolean;
 }
 
+export interface useRatingProps {
+  iosAppId: string;
+  alertTitle?: string;
+  alertMsg?: string;
+  okButton?: string;
+  cancelButton?: string;
+}
 export interface Rated {
   rated: boolean;
   appVersion: string;
